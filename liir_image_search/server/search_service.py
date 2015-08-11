f324e41579b78b6704d4671e84fad79df4b3ae5d
Anirudh Tomer <anirudhtomer@gmail.com>
2015-08-11 15:05:35 +0200

    merged with real nlp engine

--- liir_image_search/server/search_service.py
+++ liir_image_search/server/search_service.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 import json, logging, logging.config
 import random
+import liir_image_search.server.text2image.dress_demo.demo_code.txt2img.txt2img as txt2img
 
 with open("logging.json", "r") as logging_file:
     logging.config.dictConfig(json.load(logging_file))
@@ -26,19 +27,21 @@ filename2imageitem_map = {}
 for img2txtitem in img2txtdata:
     filename2imageitem_map[img2txtitem['img_filename']] = img2txtitem
 
-#CAll susana's actual function here
-def findImagesForTheText(keywords):
+def find_images_for_the_text(keywords):
     logger.debug(keywords)
-    indices = random.sample(range(0, len(predicted_imgs) - 1), TEXT2IMAGE_COUNT)
+    image_url_set=txt2img.text2image(keywords, TEXT2IMAGE_COUNT)
+
     image_set = []
-    for i in indices:
-        image_set.append({'url': predicted_imgs[i].split("data/")[1], 'score': i})
+    for imageurl in image_url_set:
+        image_set.append({'url': imageurl.split("data/")[1], 'score': 0})
+
+    logger.debug(image_set)
     return image_set
 
 @search_service.route("/search/text2image", methods=['POST'])
 def get_images():
     request_obj = request.get_json()
-    return jsonify(images=findImagesForTheText(request_obj['keywords']))
+    return jsonify(images=find_images_for_the_text(request_obj['keywords']))
 
 
 @search_service.route("/search/rawimages", methods=['POST'])
@@ -67,77 +70,10 @@ def upload_image():
     if file.filename in filename2imageitem_map:
         file.save(app_config['upload_folder'] + "/" + file.filename)
         response['imgDetails'] = filename2imageitem_map[file.filename]
-        response['similarImages'] = findImagesForTheText(response['imgDetails']['words_predicted'].split(" "))
+        response['similarImages'] = find_images_for_the_text(response['imgDetails']['words_predicted'].split(" "))
     else:
         response['failureReason'] = "We could not find details for this image"
 
     return jsonify(**response)
 
 logger.info("loaded: " + __name__)
-
-'''
- Following stuff to be removed after real binding with engine
-'''
-
-predicted_imgs = [
-    "dress_attributes/data/images/BridesmaidDresses/B00B98XYP0.jpg",
-    "dress_attributes/data/images/Wedding2/B00MLXYU1E.jpg",
-    "dress_attributes/data/images/Wedding2/B00MWH1HTW.jpg",
-    "dress_attributes/data/images/SpecialOccasion2/B00L21IJSQ.jpg",
-    "dress_attributes/data/images/Wedding/B00KFIQPQK.jpg",
-    "dress_attributes/data/images/NightOutCocktail2/B00UA8KUJS.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00N70U37U.jpg",
-    "dress_attributes/data/images/MotheroftheBrideDresses2/B00G34B8QW.jpg",
-    "dress_attributes/data/images/BridesmaidDresses2/B00M7S0TOU.jpg",
-    "dress_attributes/data/images/Dresses/B00N8IGX3O.jpg",
-    "dress_attributes/data/images/NightOutCocktail2/B00OZUR798.jpg",
-    "dress_attributes/data/images/Wedding2/B00GV176HC.jpg",
-    "dress_attributes/data/images/BridesmaidDresses2/B00QNE2208.jpg",
-    "dress_attributes/data/images/Dresses2/B00B6EXL6E.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00J7PCT9O.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00L4PNMTG.jpg",
-    "dress_attributes/data/images/Wedding2/B00M2P7298.jpg",
-    "dress_attributes/data/images/BridesmaidDresses2/B00URT8GT6.jpg",
-    "dress_attributes/data/images/Dresses2/B00NYSMK44.jpg",
-    "dress_attributes/data/images/Wedding2/B00Q6I1004.jpg",
-    "dress_attributes/data/images/Wedding2/B00NOGN6UI.jpg",
-    "dress_attributes/data/images/Wedding2/B00NKWSHNW.jpg",
-    "dress_attributes/data/images/WeddingDresses/B00BPQ1296.jpg",
-    "dress_attributes/data/images/WeddingDresses2/B0091QUA3G.jpg",
-    "dress_attributes/data/images/WeddingDresses/B00ITKF756.jpg",
-    "dress_attributes/data/images/WeddingDresses2/B00RYU3MPO.jpg",
-    "dress_attributes/data/images/WeddingDresses/B00DFKPHGY.jpg",
-    "dress_attributes/data/images/Wedding2/B00MQEZH88.jpg",
-    "dress_attributes/data/images/SpecialOccasion/B00HCXOJNM.jpg",
-    "dress_attributes/data/images/WeddingDresses/B00DHWXOGA.jpg",
-    "dress_attributes/data/images/Wedding2/B00MNZK1AO.jpg",
-    "dress_attributes/data/images/Wedding2/B00ITK6A48.jpg",
-    "dress_attributes/data/images/WeddingDresses/B009CQWU74.jpg",
-    "dress_attributes/data/images/Dresses/B00BM6U9N4.jpg",
-    "dress_attributes/data/images/Wedding/B00AXWIQWO.jpg",
-    "dress_attributes/data/images/SpecialOccasion2/B00MRTCWUS.jpg",
-    "dress_attributes/data/images/Wedding/B00G0NUKNI.jpg",
-    "dress_attributes/data/images/SpecialOccasion2/B00O3YR368.jpg",
-    "dress_attributes/data/images/Wedding2/B00URC9SMC.jpg",
-    "dress_attributes/data/images/Wedding2/B00R7E5XRW.jpg",
-    "dress_attributes/data/images/WeddingDresses/B00JVIVN62.jpg",
-    "dress_attributes/data/images/Wedding2/B00HC18Q92.jpg",
-    "dress_attributes/data/images/SpecialOccasion2/B00BMOOBAS.jpg",
-    "dress_attributes/data/images/Wedding/B00ITK8XYI.jpg",
-    "dress_attributes/data/images/WeddingDresses2/B00MA8J344.jpg",
-    "dress_attributes/data/images/Wedding2/B00N8NKMN6.jpg",
-    "dress_attributes/data/images/NightOutCocktail2/B00EPRQ9BS.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00MXRRMSG.jpg",
-    "dress_attributes/data/images/WeddingDresses2/B00UULVOZY.jpg",
-    "dress_attributes/data/images/WeddingDresses2/B00NXX1E2E.jpg",
-    "dress_attributes/data/images/Wedding/B00DCFFB18.jpg",
-    "dress_attributes/data/images/MotheroftheBrideDresses2/B00PBZ413Q.jpg",
-    "dress_attributes/data/images/Wedding/B00JVJ5X0S.jpg",
-    "dress_attributes/data/images/Wedding2/B00SAET676.jpg",
-    "dress_attributes/data/images/NightOutCocktail2/B00ITIL04A.jpg",
-    "dress_attributes/data/images/BridesmaidDresses2/B00MHDJYXW.jpg",
-    "dress_attributes/data/images/SpecialOccasion2/B00FHT82GI.jpg",
-    "dress_attributes/data/images/Dresses/B00EP6VT0U.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00K6CF4EY.jpg",
-    "dress_attributes/data/images/BridesmaidDresses/B00K5AP4JM.jpg"
-]
---
+++ liir_image_search/server/text2image/dress_demo/demo_code/txt2img/txt2img.py
@@ -0,0 +1,55 @@
+import sys
+import numpy as np
+
+from liir_image_search.server.text2image.dress_demo.demo_code.myGibbsProcess.gibbs_input import process_gibbs_input as gibbs_input
+from liir_image_search.server.text2image.dress_demo.demo_code.data_manager.data_provider import DataProvider
+
+split = 'test'
+dataset_fname = 'server/text2image/dress_demo/data/json/dataset_dress_all_{}.json'.format(split)
+dataset = DataProvider(dataset_fname=dataset_fname)
+
+
+# load the dictionary from file for word ids
+# Load true list
+rpath2 = 'server/text2image/dress_demo/data/data-processed/zappos-preprocessingfc7/test/text_visfc7/'
+dot_words_fname = rpath2 + 'text.words'
+dot_words = gibbs_input.DotWordsFile(dot_words_fname)
+# print "Vocabulary size", dot_words.V
+
+# Load matrix from model
+model_name = 'cca'
+exp = 'txt2img'
+split = 'test'
+fname = 'server/text2image/dress_demo/data/models/{}/{}_U_V_I{}.txt'.format(model_name, exp, split)
+model = np.genfromtxt(fname, delimiter=',')
+# print "model shape", model.shape
+
+
+def text2image(keywords, total_images):
+
+    # Create an Stest matrix of 1 x V and transform words into indices:
+    Stest = np.zeros((1, dot_words.V))
+    for word in keywords:
+        word_id = dot_words.word2id[word]
+        # print word_id
+        Stest[0, word_id] += 1
+
+    # print "Stest shape", Stest.shape
+
+    # Multiply Stest * a
+    img_scores = Stest.dot(model)
+    # print "img_scores shape", img_scores.shape
+
+    top_img_ids = np.argsort(-img_scores, axis=1)
+    top_img_ids = top_img_ids.tolist()
+
+    image_path_array = []
+    for img_id in top_img_ids[0][0:total_images]:
+        predicted_img = dataset.dataset['items'][img_id]
+        image_path_array.append(predicted_img['folder'] + predicted_img['img_filename'])
+
+    return image_path_array
+# print top_img_ids[0][0:5]
+
+
+
